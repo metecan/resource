dcdf091216ad0412aefa07b0f0eb412440171779
metecan <metecankaplan@hotmail.com>
2024-01-29 16:55:13 +0300

    [REFACTOR]: Add isValidUrl utility function and update image handling logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { isValidUrl } from "./utils/isValidUrl";
 import { startsWith } from "./utils/startsWith";
 
 const app = new Hono();
@@ -7,27 +8,31 @@ const port = Number(process.env.PORT || 3030);
 
 app.get("/", async (c) => {
   const targetUrl = c.req.query("src") ?? "";
-  const response = await fetch(targetUrl);
-  const header = response.headers.get("Content-Type");
 
-  if (!response.ok || !header) {
-    c.status(response.status);
+  if (targetUrl !== "" && isValidUrl(targetUrl)) {
+    const response = await fetch(targetUrl);
+    const header = response.headers.get("Content-Type");
 
-    return c.json({
-      error: "Image Not Found or Invalid URL",
-      message: "Please provide valid image url in src query parameter",
-    });
+    if (!response.ok || !header || targetUrl === undefined) {
+      c.status(response.status);
+
+      return c.json({
+        error: "Image Not Found or Invalid URL",
+        message: "Please provide valid image url in src query parameter",
+      });
+    }
+
+    if (startsWith(header, "image")) {
+      c.header(
+        "Content-Type",
+        response.headers.get("Content-Type") || "application/image"
+      );
+
+      return c.body(response.body);
+    }
   }
 
-  if (startsWith(header, "image")) {
-    c.header(
-      "Content-Type",
-      response.headers.get("Content-Type") || "application/image"
-    );
-
-    return c.body(response.body);
-  }
-
+  c.status(400);
   return c.json({
     error: "Image Not Found or Invalid URL",
     message: "Please provide valid image url in src query parameter",
